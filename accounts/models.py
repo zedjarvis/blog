732e7f6db813752173f856114c4faaad66f038d1
Cedrouse Roll <omondicedo@gmail.com>
2021-10-04 01:36:24 +0300

    quality updates, links storage and model updated

--- accounts/models.py
+++ accounts/models.py
@@ -31,7 +31,7 @@ class Profile(models.Model):
                               null=True, blank=True)
 
     profile_image = models.ImageField(default='default.png',
-                                      upload_to='users/',
+                                      upload_to='media/',
                                       null=True, blank=True)
 
     def __str__(self):
