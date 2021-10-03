6a574925c17825debe6413734636b4e0cc786b8a
Cedrouse Roll <omondicedo@gmail.com>
2021-10-04 02:10:17 +0300

    added media root

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'login'
