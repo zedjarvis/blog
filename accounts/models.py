215cf9a700cf6d64761a36ea19fa1068add5d5f9
Cedrouse Roll <54283441+zedjarvis@users.noreply.github.com>
2021-10-04 01:20:47 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,10 @@
+import io
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from lxml import etree
 from PIL import Image
 
 
